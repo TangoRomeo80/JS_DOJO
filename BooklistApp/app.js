998c608a6f1250cc88c6a437161265379add3f34
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-09-20 08:13:00 +0600

    added js functionality

--- BooklistApp/app.js
+++ BooklistApp/app.js
@@ -47,5 +47,17 @@ class UI{
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //Event: Add a book
+document.querySelector('#book-form').addEventListener('submit',(e) =>{
+   //prevent default submit
+    e.preventDefault();
+
+    //Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    //instantiate book
+    const book = new Book(title, author, isbn);
+});
 
 //Event: Remove a Book
