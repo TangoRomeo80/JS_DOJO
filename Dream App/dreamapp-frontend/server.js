546bc04bf5eaab965da8fa72f2d575f04239800c
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-04-12 09:53:37 +0600

    create server

---
+++ Dream App/dreamapp-frontend/server.js
@@ -0,0 +1,34 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.post('/dream', async (req, res) => {
+  const { prompt } = req.body
+
+  const aiResponse = await openai.createImage({
+    prompt,
+    n: 1,
+    size: '1024x1024',
+  })
+
+  const image = aiResponse.data.data[0].url
+  res.send({ image })
+})
+
+app.listen(5000, () => {
+  console.log('make art on http://localhost:5000/dream')
+})
